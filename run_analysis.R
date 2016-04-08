5408aa9e57511642a13d53bfcf2b9dd759944605
Naga1217 <rajesh1217.sas@gmail.com>
2016-04-08 16:01:19 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+###############################################################################################
 Instructions for project
 The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. 
 The goal is to prepare tidy data that can be used for later analysis. 
@@ -11,6 +12,7 @@ http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartpho
 Here are the data for the project:
 https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 After dowloading the files manually or using download.file, 
+##############################################################################################
 
 ##Read the test and training data in to R 
 #######################################################
